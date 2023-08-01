53de6777a1bef3d36ed825a01eebd18e2723c9c8
Cao Long <117249851+Clarchj@users.noreply.github.com>
2023-08-01 16:51:57 +0700

    "Validate User's Choice, Fix typos, Case-isensitve
    The program now accept case-insensitive answer."

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function getComputerChoice(){
-    const options = ["Rock", "Paper", "Scissor"];
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
-} 
+}
 
-console.log(getComputerChoice())
+function playRound(computerChoice, playerChoice) {
+    const cc = computerChoice.toLowerCase();
+    const pc = playerChoice.toLowerCase();
+
+    if (pc === cc) {
+        return "It's a tie!";
+    } else if (
+        (pc === "rock" && cc === "scissors") ||
+        (pc === "scissors" && cc === "paper") ||
+        (pc === "paper" && cc === "rock")
+    ) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+let playerSelection = prompt('Choose your option? (Rock/Paper/Scissors)').trim();
+
+// Validate user input
+const validOptions = ["rock", "paper", "scissors"];
+while (!validOptions.includes(playerSelection.toLowerCase())) {
+    playerSelection = prompt('Invalid choice! Please choose between Rock, Paper, or Scissors.').trim();
+}
+
+const computerSelection = getComputerChoice();
+const result = playRound(computerSelection, playerSelection);
+
+console.log("Computer's choice:", computerSelection);
+console.log("Your choice:", playerSelection);
+alert(result);
