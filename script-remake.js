af8625e435991bad7cb818f0065e36a61a175b97
Cao Long <117249851+Clarchj@users.noreply.github.com>
2023-08-27 22:35:37 +0700

    "new feture: congratz player on winning"

--- script-remake.js
+++ script-remake.js
@@ -1,45 +1,65 @@
 //this function will generate random R/P/S for the computer:
 
-const playerText = document.querySelector('.player-score');
-const computerText =document.querySelector('.computer-score');
-const resultText=document.querySelector('#result')
+const playerScoreText = document.querySelector('.player-score .score');
+const computerScoreText = document.querySelector('.computer-score .score');
+const resultText = document.querySelector('#result')
 let playerChoice;
 let computerChoice;
 let playerScore;
 let computerScore;
 let result;
+playerScore = 0;
+computerScore = 0;
 
-const getComputerChoice = function(){
+const getComputerChoice = function () {
     const options = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
-    return options[randomIndex].toString();   
+    return options[randomIndex].toString();
 }
+computerChoice = getComputerChoice();
+
 function checkWinner(cc, pc) {
     cc = cc.toLowerCase();
     pc = pc.toLowerCase();
-
-    if (pc === cc) {
-        result = `It's a tie! You both chose ${pc}.`;
-    } else if (
-        (pc === "rock" && cc === "scissors") ||
-        (pc === "scissors" && cc === "paper") ||
-        (pc === "paper" && cc === "rock")
-    ) {
-        result = `You win! ${pc} beats ${cc}.`;
-    } else {
-        result = `You lose! ${cc} beats ${pc}.`;
+    {
+        if (pc === cc) {
+            result = `It's a tie! You both chose ${pc}.`;
+        } else if (
+            (pc === "rock" && cc === "scissors") ||
+            (pc === "scissors" && cc === "paper") ||
+            (pc === "paper" && cc === "rock")
+        ) {
+            result = `You win! ${pc} beats ${cc}.`;
+            playerScore++;
+        } else {
+            result = `You lose! ${cc} beats ${pc}.`;
+            computerScore++;
+        }
     }
 }
 
-document.querySelectorAll('.box').forEach( choice =>{
+document.querySelectorAll('.box').forEach(choice => {
     choice.addEventListener('click', () => {
         playerChoice = choice.getAttribute('data-choice');
+        computerChoice = getComputerChoice();
         // const result = playRound(getComputerChoice(), playerChoice);
         // displayResult(result);
         console.log(playerChoice);
+        checkWinner(computerChoice, playerChoice);
+        resultText.textContent = result;
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+        if (playerScore === 5) {
+            resultText.textContent = "Player wins! Glory for the king!";
+        }
+        if (computerScore === 5) {
+            resultText.textContent = "Computer wins this battle! You have been defeated";
+        }
     });
 
-} )
+})
+
+
 
 
 
